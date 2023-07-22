8b7c4168a93802ddc50854ea127f12000ad55333
Artem Yakubovich <artempaulavich@gmail.com>
2023-07-22 11:33:00 +0200

    add Player animations Idle, Walking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
+
+    private bool _isWalking;
     
     private void Update()
     {
@@ -29,7 +31,13 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * _moveSpeed * Time.deltaTime;
 
+        _isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return _isWalking;
     }
 }
