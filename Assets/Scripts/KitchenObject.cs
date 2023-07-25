73d12bd6bf2deb3e0532d374ad80e5cb6a0f11e9
Artem Yakubovich <artempaulavich@gmail.com>
2023-07-25 10:24:55 +0200

    add Kitchen Object parent

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -6,8 +6,34 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO _kitchenObjectSO;
 
+    private ClearCounter _clearCounter;
+    
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return _kitchenObjectSO;
+    }
+    
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if (this._clearCounter != null)
+        {
+            this._clearCounter.ClearKitchenObject();
+        }
+        this._clearCounter = clearCounter;
+
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Counter already has KitchenObject!");
+        }
+        
+        clearCounter.SetKitchenObject(this);
+        
+        transform.parent = _clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return _clearCounter;
     }
 }
